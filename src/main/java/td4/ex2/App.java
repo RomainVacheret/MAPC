7260dea5022540354f760819767bcd7bd0b1f69f
Romain Vacheret <romain.vacheret@gmail.com>
2020-11-10 17:20:51 +0100

    Battle

--- src/main/java/td4/ex2/App.java
+++ src/main/java/td4/ex2/App.java
@@ -1,5 +1,9 @@
 package td4.ex2;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
         DeplacementStrategy rouler = Rouler.getInstance();
@@ -15,15 +19,29 @@ public class App {
         Unite avion = new Unite(voler, missile, 4);
         Unite char_ = new Unite(rouler, canon, 3);
         Unite charModifie = new Unite(rouler, lanceFlamme, 3);
+
+        List<Unite> unites = List.of(soldat, avion, char_, charModifie);
+
+        List<Unite> unitesVivantes = unites;
+        int idx = new Random().nextInt(unites.size());
+        System.out.println(idx);
         
-        soldat.seDeplacerEtAttaquer(char_);
-        avion.seDeplacerEtAttaquer(charModifie);
-        char_.seDeplacerEtAttaquer(avion);
-        charModifie.seDeplacerEtAttaquer(char_);
+        do {
+            int autreIdx = new Random().nextInt(unitesVivantes.size());
+            unitesVivantes.get(idx).attaquer(unitesVivantes.get(autreIdx));
+            unitesVivantes = unites.stream().filter(Unite::estVivant).collect(Collectors.toList());
+            idx = (idx + 1) % unitesVivantes.size();
+        } while(unitesVivantes.size() != 1);
+        
+        // soldat.seDeplacerEtAttaquer(char_);
+        // avion.seDeplacerEtAttaquer(charModifie);
+        // char_.seDeplacerEtAttaquer(avion);
+        // charModifie.seDeplacerEtAttaquer(char_);
 
         System.out.println(soldat.estVivant());
         System.out.println(avion.estVivant());
         System.out.println(char_.estVivant());
         System.out.println(charModifie.estVivant());
+
     }
 }
